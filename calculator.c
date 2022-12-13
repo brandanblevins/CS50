efc9394ea9e1832e941feb5ccc4b355296d6914c
brandanblevins <brandan.blevins@gmail.com>
2022-12-13 10:49:37 -0700

    Add basic calculator

---
+++ calculator.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int x = get_int("x: ");
+    int y = get_int("y: ");
+    printf("%i\n");
+}
+
+// %i = gives you a variable for an integer.
