adc231a6991825d0571fd216a0c425d86daac76b
brandanblevins <brandan.blevins@gmail.com>
2022-12-13 09:31:53 -0700

    Add check in point

--- hello.c
+++ hello.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
+#include <cs50.h>
+#include <stdio.h> // Standard library that gives access to functions like 'printf'.
 
 int main(void)
 {
     printf("hello, world\n");
+}
+
+int main(void)
+{
+    string answer = get_string("What's your name? ");
+    printf("hello, answer\n");
 }
