b862b3671c3cd33756c4e39ae749b423439756ef
brandanblevins <brandan.blevins@gmail.com>
2022-12-13 11:10:32 -0700

    Check in parity

---
+++ parity.c
@@ -0,0 +1,18 @@
+#include
+#include
+
+int main(void)
+{
+    int n = get_int("n: ");
+
+    // If n is even
+    if (???)
+    {
+        printf("even\n");
+    }
+    // Else
+    else (???)
+    {
+    printf("odd\n");
+    }
+}
