8802f49aaf0cc913969b3fedbfec3f076d528a82
brandanblevins <brandan.blevins@gmail.com>
2022-12-15 15:18:21 -0700

    Clean up uppercase example with toupper

--- Week 2/uppercase.c
+++ Week 2/uppercase.c
@@ -9,13 +9,7 @@ int main(void)
     printf("After: ");
     for (int = 0, n = strlen(s); i < n; i++)
     {
-        if (islower(s[i])) // Professor basically used the ascii chart to calculate the spot of lowercase letters, then subtracted 32 to get to the uppercase versions.
-        {
             printf("%c", toupper(s[i]);
-        }
-        else
-        {
-            printf("%c", s[i]);
         }
         printf("\n");
     }
