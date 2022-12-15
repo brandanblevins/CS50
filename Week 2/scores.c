6f8a87cb72e09ab61665d9cc9e79a5b02dbeba6c
brandanblevins <brandan.blevins@gmail.com>
2022-12-15 14:21:02 -0700

    Update array

--- Week 2/scores.c
+++ Week 2/scores.c
@@ -4,9 +4,9 @@ int main(void)
 {
    int scores[3];
 
-    scores[0] = 72;
-    scores[1] = 73;
-    scores[2] = 33;
+    scores[0] = get_int("Score: ");
+    scores[1] = get_int("Score: ");
+    scores[2] = get_int("Score: ");
 
     printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
