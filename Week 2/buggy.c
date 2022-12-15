aef7bb4d68390f55e0ba84f12a00753ef7a38813
brandanblevins <brandan.blevins@gmail.com>
2022-12-15 14:03:14 -0700

    Add buggy version of buggy.c

--- Week 2/buggy.c
+++ Week 2/buggy.c
@@ -2,9 +2,17 @@
 
 int main(void)
 {
-    for (int i = 0; i <= 3; i++)
-        {
-            printf("i is %i\n", i);
-            printf("#]n");
-        }
+    int i = get_negative_int();
+    printf("%i\n", i);
+}
+
+int get_negative_int(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Negative Integer: ");
+    }
+    while (n < 0);
+    return n;
 }
