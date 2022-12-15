a9b5547f09e8eb509edba53607849bc950f0ba6c
brandanblevins <brandan.blevins@gmail.com>
2022-12-15 14:52:27 -0700

    Comment length example

--- Week 2/length.c
+++ Week 2/length.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-    string name = get_string("Name: ");
+    string name = get_string("Name: "); // This prompts the user to enter a name.
 
-    int i = 0
-    while (name[i] != \0)
+    int i = 0; // This established the i variable and sets it to 0.
+    while (name[i] != \0) // A while loop. the \0 is the equivalent of \n, so a break.
     {
-        i++;
+        i++; // Adds 1 to i every time the while loops.
     }
-    printf("%i\n", i);
+    printf("%i\n", i); // Prints i.
 }
