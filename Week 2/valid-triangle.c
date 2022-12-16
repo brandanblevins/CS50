6fcb932f22a29a0a23ae0de78e506ac257fd25eb
brandanblevins <brandan.blevins@gmail.com>
2022-12-16 08:25:30 -0700

    Add correct answer for valid-triangle

--- Week 2/valid-triangle.c
+++ Week 2/valid-triangle.c
@@ -1,3 +1,29 @@
+// Correct version.
+
+#include <stdio.h>
+#include <cs50.h>
+
+bool valid_triangle(float x, float y, float z);
+
+bool valid_triangle(float x, float y, float z)
+{
+    // Check for all positive sides.
+    if (x <= 0 || y <= 0 || z <= 0)
+    {
+        return false;
+    }
+
+    // Check the sum of any two sides greater than third.
+    if ((x + y <= z) || (x + z <= y) || (y + z <= x))
+    {
+        return false;
+    }
+
+    // If we passed both tests, we are good!
+    return true;
+}
+
+
 // My attempt at the valid triangle exercise.
 
 #include <stdio.h>
@@ -24,5 +50,5 @@ bool main(void)
 
 bool check_sides();
 {
-    
+
 }
