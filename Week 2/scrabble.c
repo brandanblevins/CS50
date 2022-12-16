efb39da47fbca4a44db64448508343133d1ad9d2
brandanblevins <brandan.blevins@gmail.com>
2022-12-16 12:24:53 -0700

    Add my solution for printing the winner in scrabble

--- Week 2/scrabble.c
+++ Week 2/scrabble.c
@@ -19,6 +19,16 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+        if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    } else if (score2 > score1)
+    {
+        printf("Player 2 wins!\n");
+    } else if (score2 == score1)
+    {
+        printf("It's a tie!\n");
+    }
 }
 
 int compute_score(string word)
