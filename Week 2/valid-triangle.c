db5f06e2bb85f3cf2d668301ab388a99d317188e
brandanblevins <brandan.blevins@gmail.com>
2022-12-16 07:58:33 -0700

    Check in progress on valid-triangle

--- Week 2/valid-triangle.c
+++ Week 2/valid-triangle.c
@@ -4,9 +4,20 @@
 #include <cs50.h>
 
 bool valid_triangle();
+bool side_1_2();
+bool side_2_3();
+bool side_1_3();
 
-int main(void)
+bool main(void)
+{
+    int side_1 = get_int("Give me the first side: ");
+    int side_2 = get_int("Give me the second side: ");
+    int side_3 = get_int("Give me the third side: ");
+
+    return valid_triangle;
+}
+
+bool side_1_2();
 {
 
 }
-
